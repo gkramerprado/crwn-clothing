bbf5c04f54379ba228381c444216b2f5547802d6
Guilherme Kramer <guilherme.kramer98@gmail.com>
2020-01-11 10:28:32 -0300

    criado componente Header com rotas

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import Homepage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
-import { Route } from 'react-router-dom';
+import Header from './components/header/header.component';
+
+import { Route, Switch } from 'react-router-dom';
 
 import './App.css';
 
@@ -17,9 +19,12 @@ const HatsPage = (props) => {
 function App() {
   return (
     <>
-      <Route exact path='/' component={Homepage} />
-      <Route exact path='/shop' component={ShopPage} />
-      <Route exact path='/shop/chapeus' component={HatsPage} />
+      <Header />
+      <Switch>
+        <Route exact path='/' component={Homepage} />
+        <Route exact path='/shop' component={ShopPage} />
+        <Route exact path='/shop/chapeus' component={HatsPage} />
+      </Switch>
     </>
   );
 }
