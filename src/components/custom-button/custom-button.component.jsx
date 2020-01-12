23ec1e7ff5709fb9f3a1b2c9f8f097e3d992a532
Guilherme Kramer <guilherme.kramer98@gmail.com>
2020-01-12 14:49:40 -0300

    adicionado estilo aos card-items e custom button inverted

--- src/components/custom-button/custom-button.component.jsx
+++ src/components/custom-button/custom-button.component.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 
 import './custom-button.styles.scss';
 
-const CustomButton = ({ children, isGoogle, ...otherProps }) => {
+const CustomButton = ({ children, isGoogle, inverted, ...otherProps }) => {
     return (
-        <button className={`${isGoogle ? 'google-sign-in' : ''} custom-button`} {...otherProps}>
+        <button className={`${inverted ? 'inverted' : ''} custom-button`} {...otherProps}>
             {children}
         </button>
     );
