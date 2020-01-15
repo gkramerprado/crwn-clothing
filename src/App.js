e306c1d6a387821c49aed6afc207a72359015837
Guilherme Kramer <guilherme.kramer98@gmail.com>
2020-01-14 22:34:31 -0300

    refatorado codigo para melhorar performance utilizando o reselect ao renderizar os states do redux

--- src/App.js
+++ src/App.js
@@ -7,6 +7,8 @@ import { auth, createUserProfileDocument } from './firebase/firebase.utils';
 import { Route, Switch, Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { setCurrentUser } from './redux/user/user.actions';
+import { selectCurrentUser } from './redux/user/user.selectors';
+import { createStructuredSelector } from 'reselect';
 
 import './App.css';
 
@@ -69,8 +71,8 @@ class App extends React.Component {
   }
 }
 
-const mapStateToProps = ({ user }) => ({
-  currentUser: user.currentUser,
+const mapStateToProps = createStructuredSelector({
+  currentUser: selectCurrentUser,
 });
 
 const mapDispatchToProps = dispatch => ({
