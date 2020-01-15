adc0e2714868b436e6ec70e69f42c49eabd99a7d
Guilherme Kramer <guilherme.kramer98@gmail.com>
2020-01-15 20:59:26 -0300

    Adicionado persistor para armazenar os dados no local storage

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import store from './redux/store';
+import { store, persistor } from './redux/store';
+import { PersistGate } from 'redux-persist/integration/react';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 
@@ -10,7 +11,9 @@ import './index.css';
 ReactDOM.render(
     <Provider store={store}>
         <BrowserRouter>
-            <App />
+            <PersistGate persistor={persistor}>
+                <App />
+            </PersistGate>
         </BrowserRouter>
     </Provider>,
     document.getElementById('root')
