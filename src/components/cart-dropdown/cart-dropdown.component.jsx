2a5536b87536f7492475935315621e762cad3512
Guilherme Kramer <guilherme.kramer98@gmail.com>
2020-01-14 21:24:30 -0300

    criado componente de item para o carrinho

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,16 +1,28 @@
 import React from 'react';
-
 import CustomButton from '../custom-button/custom-button.component';
+import CartItem from '../cart-item/cart-item.component';
+
+import { connect } from 'react-redux';
 
 import './cart-dropdown.styles.scss';
 
-const Cart = () => {
+const Cart = ({ cartItems }) => {
     return (
         <div className="cart-dropdown">
-            <div className="cart-items"></div>
+            <div className="cart-items">
+                {
+                    cartItems.map(cartItem => (
+                        <CartItem key={cartItem.id} item={cartItem} />
+                    ))
+                }
+            </div>
             <CustomButton> FINALIZAR </CustomButton>
         </div>
     );
 };
 
-export default Cart;
+const mapStateToProps = ({ cart: { cartItems } }) => ({
+    cartItems,
+});
+
+export default connect(mapStateToProps)(Cart);
