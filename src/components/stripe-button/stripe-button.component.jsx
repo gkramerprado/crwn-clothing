ce704af66607c4e686134ca7c7f0f905896a3485
Guilherme Kramer <guilherme.kramer98@gmail.com>
2020-01-25 15:59:21 -0300

    Adicionada ligacao para pagamento com a API do Stripe

---
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import StripeCheckout from 'react-stripe-checkout';
+
+const StripeCheckoutButton = ({ price }) => {
+    const priceForStripe = price * 100;
+    const publishableKey = 'pk_test_tWx2vdYApcDjBotmLkjg2Xz600wDpEjDwy';
+
+    const onToken = token => {
+        console.log(token);
+        alert("Pagamento efetuado com sucesso !");
+    }
+
+    return (
+        <StripeCheckout
+            label="Pagar Agora"
+            name="CRWN Roupas Ltd."
+            billingAddress
+            shippingAddress
+            image="https://svgshare.com/i/CUz.svg"
+            description={`Total a pagar: $ ${price}`}
+            amount={priceForStripe}
+            panelLabel="Pagar Agora"
+            token={onToken}
+            stripeKey={publishableKey}
+        />
+    )
+}
+
+export default StripeCheckoutButton;
